bcdec2a3b357c8d0a0358348426a3ac25fa84875
DigoRM <rodrigormarcolino@gmail.com>
2023-02-15 14:49:26 -0300

    User Registration, Django messages and errors

--- accounts/signals.py
+++ accounts/signals.py
@@ -7,17 +7,15 @@ from .models import User, UserProfile
 def post_save_create_profile_receiver(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-        print('Created the user profile')
     else:
         try:
             profile = UserProfile.objects.get(user=instance)
             profile.save()
         except:
             UserProfile.objects.create(user=instance)
-            print("Profile didn't exist, I created one now")
-        print("User updated")
+
 
 @receiver(pre_save, sender=User)
 def pre_save_profile_receiver(sender, instance, **kwargs):
-    print(instance.username, 'This user had been saved')
+    pass
 #post_save.connect(post_save_create_profile_receiver, sender=User)
